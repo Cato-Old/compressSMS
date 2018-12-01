f55e766bf65cc5f438e7d229ed63006fe8d9b8d0
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2018-12-01 13:02:10 +0100

    implementing test

--- src/test/java/Compressor/OptimalCompressorTest.java
+++ src/test/java/Compressor/OptimalCompressorTest.java
@@ -1,1 +1,20 @@
-package Compressor;import org.junit.Before;import org.junit.Test;import static org.junit.Assert.*;public class OptimalCompressorTest {    private OptimalCompressor compr;    @Before    public void setUp(){        compr = new OptimalCompressor();    }    @Test    public void optimalTest(){        String testString = "Ala ma kota";        assertTrue(testString.length() > compr.compress(testString).length());    }}
+package Compressor;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class OptimalCompressorTest {
+    private OptimalCompressor compr;
+
+    @Before
+    public void setUp(){
+        compr = new OptimalCompressor();
+    }
+
+    @Test
+    public void optimalTest(){
+        String testString = "Ala ma kota";
+        assertTrue(testString.length() > compr.compress(testString).length());
+    }
+}
--- src/test/java/PaginatorTest.java
+++ src/test/java/PaginatorTest.java
@@ -1,1 +1,17 @@
-import org.junit.Before;import org.junit.Test;import static org.junit.Assert.*;public class PaginatorTest {    private Paginator pagin;    @Before    public void setUp(){        pagin = new Paginator(160);    }    @Test    public void paginateTest(){        assertEquals(2,pagin.paginate("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").length);    }}
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PaginatorTest {
+    private Paginator pagin;
+
+    @Before
+    public void setUp(){
+        pagin = new Paginator(160);
+    }
+
+    @Test
+    public void paginateTest(){
+        assertEquals(2,pagin.paginate("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").length);
+    }
+}
