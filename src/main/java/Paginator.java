380bc391d7ad9436f6c7cb0b85cf5fc4c77b66e9
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2018-12-04 15:12:51 +0100

    implementing method paginate in class Paginator

--- src/main/java/Paginator.java
+++ src/main/java/Paginator.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Paginator {
     private final int SMS_LENGHT;
 
@@ -6,6 +9,14 @@ public class Paginator {
     }
 
     public String[] paginate(String text){
-        return null;
+        List<String> pages = new ArrayList<>();
+        for (int i = 0; i < text.length(); i += SMS_LENGHT){
+            if (text.length() - i > SMS_LENGHT){
+                pages.add(text.substring(i,i+SMS_LENGHT));
+            } else {
+                pages.add(text.substring(i));
+            }
+        }
+        return pages.toArray(new String[0]);
     }
 }
